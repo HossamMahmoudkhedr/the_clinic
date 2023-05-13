2bcb30881192cfda0ee1282f6e9680bb4f8ff767
Hossam Mahmoud <nhem318@gmail.com>
2023-05-13 18:53:26 +0200

    add cookies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,19 @@ import Header from './components/header/header.jsx';
 import FullPage from './components/fullPage/fullPage';
 import { Login } from '@mui/icons-material';
 import LoginPage from './components/login/login';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 
 function App() {
+	const [name, setName] = useState();
+	useEffect(() => {
+		setName(Cookies.get('logininfo'));
+	}, [name]);
+
 	return (
 		<div className="App">
 			<Header />
-			{/* <FullPage /> */}
-			<LoginPage />
+			{name == '' ? <LoginPage /> : <FullPage />}
 		</div>
 	);
 }
